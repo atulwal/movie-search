34edfcc03cf189dfee4969d19929762eda34331a
atulwal <jaiswalatulya741@gmail.com>
2025-08-22 22:34:27 +0530

    first working version of website

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const apiKey = "9f6e2818"; 
-const movieInput = document.querySelector(".searchbar");
+const movieInput = document.querySelector(".search-bar");
 const searchBtn = document.querySelector(".search-btn");
-
+document.querySelector(".search-results").style.display = "none";
 async function getMovie(query) {
     const apiUrl = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
-    
     const response = await fetch(apiUrl);
     const data = await response.json();
-
-    console.log(data); 
-    return data;
+    const resultsDiv = document.querySelector(".results-flex")
+    resultsDiv.innerHTML = "";
+    console.log(data);
+    document.querySelector(".movie-cards").style.display = "none";
+    document.querySelector(".search-results").style.display = "flex";
+    if (data.Search) {
+        data.Search.forEach(movie => {
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("card");
+            movieCard.innerHTML = `
+            <img src="${movie.Poster != "N/A" ? movie.Poster : "placeholder.jpg"}" alt="${movie.Title}">
+            <div class="card-content">
+                <h3>${movie.Title}</h3>
+                <p class="date">${movie.Year}</p>
+            </div>
+        </div>`;
+        resultsDiv.appendChild(movieCard);
+        });
+    } 
+    else {
+        resultsDiv.innerHTML = `<p>No results found</p>`;
+    }
+    
 }
-
-getMovie("Swades");
 
 searchBtn.addEventListener("click", () => {
     const query = movieInput.value.trim();
     if (query) {
         getMovie(query);
+    }
+});
+
+movieInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const query = movieInput.value.trim();
+        if (query) {
+            getMovie(query);
+        }
     }
 });
